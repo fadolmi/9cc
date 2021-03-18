6727c495887a824c7c4a29e659f0acfb65fc4463
fadolmi <littlebeatskey@gmail.com>
2021-03-18 12:46:32 +0000

    ファイルを分割

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "9cc.h"
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+	error("引数の個数が正しくありません");
+	return 1;
+  }
+
+  // トークナイズしてパースする
+  user_input = argv[1];
+  token = tokenize();
+  Node* node = expr();
+
+  // アセンブリを出力
+  codegen(node);
+  return 0;
+}
